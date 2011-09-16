ae1c5710aaa36dd461d900d16fe939d54d0f8145
Stefan Glienke <sglienke@dsharp.org>
2011-09-16 09:07:43 +0000

    added search path for DSharp units to AOP sample

--- Samples/AOP/AOPDemo.dpr
+++ Samples/AOP/AOPDemo.dpr
@@ -1,14 +1,10 @@
 program AOPDemo;
 
+{$RTTI EXPLICIT METHODS([vcPrivate..vcPublished])}
+
 uses
   Forms,
-  Main in 'Main.pas' {Form1},
-  DSharp.Core.Aspects in '..\..\Source\Core\DSharp.Core.Aspects.pas',
-  DSharp.Core.Reflection in '..\..\Source\Core\DSharp.Core.Reflection.pas',
-  DSharp.Core.Aspects.Logging in '..\..\Source\Core\DSharp.Core.Aspects.Logging.pas',
-  DSharp.Core.Logging in '..\..\Source\Core\DSharp.Core.Logging.pas',
-  DSharp.Core.Logging.SmartInspect in '..\..\Source\Core\DSharp.Core.Logging.SmartInspect.pas',
-  DSharp.Core.Logging.SmartInspect.Helper in '..\..\Source\Core\DSharp.Core.Logging.SmartInspect.Helper.pas';
+  Main in 'Main.pas' {Form1};
 
 {$R *.res}
 
--- Samples/AOP/Main.pas
+++ Samples/AOP/Main.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, DSharp.Core.Aspects;
+  Dialogs, StdCtrls;
 
 type
   TForm1 = class(TForm)
@@ -25,11 +25,21 @@ implementation
 {$R *.dfm}
 
 uses
+  DSharp.Core.Aspects,
   DSharp.Core.Aspects.Logging,
+// uncomment the following line if you dont have SmartInspect - use Console logging instead
+// support for CodeSite coming soon
   DSharp.Core.Logging.SmartInspect;
+//  DSharp.Core.Logging.Console;
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
+  // following code adds the logging aspect to every virtual method that is
+  // accessable by RTTI (public and published by default)
+
+  // using the $RTTI compiler directive in your own units can make
+  // private and protected methods accessable so aspects can be weaved into
+
 //  AspectWeaver.AddAspect(TForm1, TLoggingAspect, '.*');
 //  AspectWeaver.AddAspect(TForm1, TLoggingAspect, '^((?!InitiateAction).)*$');
   AspectWeaver.AddAspect(TButton, TLoggingAspect, '.*');

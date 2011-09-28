32460befc871a1559075bc99355a384c052b8f8e
Stefan Glienke <sglienke@dsharp.org>
2011-09-28 18:07:30 +0000

    - added cleaning _history, debug and release folders (yay batch scripting!)

--- clean.bat
+++ clean.bat
@@ -1,6 +1,13 @@
-del *.~* /s
-del *.local /s
-del *.identcache /s
-del *.dcu /s
-del *.exe /s
-pause
+@echo off
+echo Cleaning files ...
+echo.
+del *.~* /s/q >nul 2>&1
+del *.local /s/q >nul 2>&1
+del *.identcache /s/q >nul 2>&1
+del *.dcu /s/q >nul 2>&1
+del *.exe /s/q >nul 2>&1
+
+echo Removing build folders ...
+echo.
+for /d /r . %%d in (__history Debug Release) do @if exist "%%d" echo "%%d" && rd /s/q "%%d"
+echo Done!

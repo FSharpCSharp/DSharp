044f1356f589bce395a110045149acf46ba9a2bf
Stefan Glienke <sglienke@dsharp.org>
2011-12-02 21:45:54 +0000

    - changed regex method call to compile sample in Delphi 2010

--- Samples/MVVM/ContactManager/ContactDetails.ViewModel.pas
+++ Samples/MVVM/ContactManager/ContactDetails.ViewModel.pas
@@ -34,7 +34,7 @@ const
     '\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\' +
     '.)+))([a-zA-Z]{2,5}|[0-9]{1,3})(\]?)$';
 begin
-  Result := TRegex.Create(CRegex).IsMatch(s);
+  Result := TRegex.IsMatch(s, CRegex);
 end;
 
 { TContactDetailsViewModel }

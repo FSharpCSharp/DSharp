d79861d4adbeac5817d06faa2d579004c71b17ac
Stefan Glienke <sglienke@dsharp.org>
2012-04-11 10:14:56 +0000

    - sample for unit tests with attributes

---
+++ Samples/UnitTesting/TestCaseAttributes.dpr
@@ -0,0 +1,24 @@
+program TestCaseAttributes;
+
+{$IFDEF CONSOLE_TESTRUNNER}
+{$APPTYPE CONSOLE}
+{$ENDIF}
+
+uses
+  Forms,
+  TestFramework,
+  GUITestRunner,
+  TextTestRunner,
+  TestCaseWithAttributes in 'TestCaseWithAttributes.pas';
+
+{$R *.RES}
+
+begin
+  Application.Initialize;
+  if IsConsole then
+    with TextTestRunner.RunRegisteredTests do
+      Free
+  else
+    GUITestRunner.RunRegisteredTests;
+end.
+
---
+++ Samples/UnitTesting/TestCaseWithAttributes.pas
@@ -0,0 +1,76 @@
+unit TestCaseWithAttributes;
+
+interface
+
+uses
+  DSharp.Testing.DUnit;
+
+type
+  UserPasswordTestCaseAttribute = class(TestCaseAttribute)
+  public
+    constructor Create(const AUserName, APassword: string; AResponse: Boolean);
+  end;
+
+  UserAgeTestCaseAttribute = class(TestCaseAttribute)
+  public
+    constructor Create(const AUserName: string; AAge: Integer; AResponse: Boolean);
+  end;
+
+  TLoginTestCase = class(TTestCase)
+  published
+    [UserPasswordTestCase('User1', 'Password1', True)]
+    [UserPasswordTestCase('User2', 'Password2', True)]
+    [UserPasswordTestCase('User3', 'Password3', True)]
+    [UserPasswordTestCase('User3', '', False)]
+    procedure TestUserLogin(const AUserName, APassword: string; AResponse: Boolean);
+    [UserAgeTestCase('User1', 26, True)]
+    [UserAgeTestCase('User2', 27, True)]
+    [UserAgeTestCase('User3', 28, False)]
+    procedure TestUserAge(const AUserName: string; AAge: Integer; AResponse: Boolean);
+  end;
+
+implementation
+
+uses
+  TestFramework;
+
+{ UserPasswordTestCaseAttribute }
+
+constructor UserPasswordTestCaseAttribute.Create(const AUserName, APassword: string;
+  AResponse: Boolean);
+begin
+  SetLength(FValues, 3);
+  FValues[0] := AUserName;
+  FValues[1] := APassword;
+  FValues[2] := AResponse;
+end;
+
+{ UserAgeTestCaseAttribute }
+
+constructor UserAgeTestCaseAttribute.Create(const AUserName: string; AAge: Integer;
+  AResponse: Boolean);
+begin
+  SetLength(FValues, 3);
+  FValues[0] := AUserName;
+  FValues[1] := AAge;
+  FValues[2] := AResponse;
+end;
+
+{ TLoginTestCase }
+
+procedure TLoginTestCase.TestUserAge(const AUserName: string; AAge: Integer;
+  AResponse: Boolean);
+begin
+
+end;
+
+procedure TLoginTestCase.TestUserLogin(const AUserName, APassword: string;
+  AResponse: Boolean);
+begin
+
+end;
+
+initialization
+  RegisterTest(TLoginTestCase.Suite);
+
+end.

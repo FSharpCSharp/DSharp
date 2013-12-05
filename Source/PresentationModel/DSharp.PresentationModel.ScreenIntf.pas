a5083f54582a7ed96c736145c35f53429209f094
Marko Vončina <marko444@gmail.com>
2013-12-05 08:34:01 +0100

    Changed IScreen to inherit from INotifyPropertyChangedEx (since multi inheritance with interfaces is not possible this is likely the best choice)

--- Source/PresentationModel/DSharp.PresentationModel.ScreenIntf.pas
+++ Source/PresentationModel/DSharp.PresentationModel.ScreenIntf.pas
@@ -2,13 +2,16 @@ unit DSharp.PresentationModel.ScreenIntf;
 
 interface
 
+uses
+  DSharp.PresentationModel.NotifyPropertyChangedExIntf;
+
 type
   ///	<summary>
   ///	  Denotes an instance which implements <see cref="IHaveDisplayName" />,
   ///	  <see cref="IActivate" />, <see cref="IDeactivate" />,
   ///	  <see cref="IGuardClose" />and <see cref="INotifyPropertyChangedEx" />
   ///	</summary>
-  IScreen = interface
+  IScreen = interface(INotifyPropertyChangedEx)
     ['{F9296F6C-AF54-4049-8D84-6C187D081710}']
   end;
 
